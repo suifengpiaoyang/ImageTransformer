669ee2caa5335648e41a5d13f49d5e5c4ab9a8f3
Zhang <504947985@qq.com>
2024-08-18 23:25:04 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Webp2jpg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ViewHelpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(25, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(487, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(541, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "转换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(719, 99);
+            this.panel1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(754, 32);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ViewHelpMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // ViewHelpMenuItem
+            // 
+            this.ViewHelpMenuItem.Name = "ViewHelpMenuItem";
+            this.ViewHelpMenuItem.Size = new System.Drawing.Size(182, 34);
+            this.ViewHelpMenuItem.Text = "使用须知";
+            this.ViewHelpMenuItem.Click += new System.EventHandler(this.ViewHelpMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 180);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Webp2jpg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ViewHelpMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+
+namespace Webp2jpg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text.Trim();
+            // 需要对路径进行验证
+            if (Directory.Exists(path))
+            {
+                TransformToJpg(path, isDirectory:true);
+            }
+            else if (File.Exists(path))
+            {
+                if (path.EndsWith(".webp"))
+                {
+                    TransformToJpg(path);
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "不支持这种格式的文件",
+                        "提示",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "文件或文件夹不存在",
+                    "提示",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+
+        private void TransformOneImageToJpg(string path, string outputDirectory=null, bool showMessage=true)
+        {
+            using (MagickImage image = new MagickImage(path))
+            {
+                image.Format = MagickFormat.Jpeg;
+                if (outputDirectory == null)
+                {
+                    outputDirectory = Path.GetDirectoryName(path);
+                }
+                string filename = Path.GetFileName(path);
+                filename = filename.Split('.')[0] + ".jpg";
+                string filepath = Path.Combine(outputDirectory, filename);
+                image.Write(filepath);
+                if (showMessage)
+                {
+                    MessageBox.Show($"{filepath} 保存成功。", "提示");
+                }
+            }
+        }
+
+        private void TransformToJpg(string path, bool isDirectory=false, bool showMessage=true)
+        {
+            if (!isDirectory)
+            {
+                TransformOneImageToJpg(path);
+            }
+            else
+            {
+                List<string> images = new List<string>();
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    if (file.EndsWith(".webp"))
+                    {
+                        images.Add(file);
+                    }
+                }
+                if (images.Count == 0) 
+                {
+                    MessageBox.Show($"在[{path}]路径下没有找到.webp后缀的图片。", "提示");
+                }
+                else
+                {
+                    string outputDirectory = path + "_transform";
+                    if (!Directory.Exists(outputDirectory))
+                    {
+                        Directory.CreateDirectory(outputDirectory);
+                    }
+                    foreach (string image in images) 
+                    {
+                        TransformOneImageToJpg(image, outputDirectory, showMessage: false);
+                    }
+                    MessageBox.Show($"{outputDirectory} 保存成功。", "提示");
+                }
+            }
+        }
+
+        private void ViewHelpMenuItem_Click(object sender, EventArgs e)
+        {
+            HelpForm helpform = new HelpForm();
+            helpform.ShowDialog();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0) 
+            {
+                textBox1.Text = files[0];
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+        }
+    }
+}
